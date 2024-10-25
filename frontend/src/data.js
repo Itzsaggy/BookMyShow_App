a6eebefd51f92ef8ac5134eb81898e88db953d35
Itzsaggy <sagarwankhede074@gmail.com>
2024-10-25 15:22:50 +0530

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,6 @@
+let moviesList = ['13 Going on 30', '10 Things I Hate About You ', 'Love & Other Drugs ', 'Crazy, Stupid, Love', 'Set It Up'];
+let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"];
+let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+exports.moviesList = moviesList;
+exports.slots = slots;
+exports.seats = seats;
